d95377250380f72f192c75309249abce96d0a5f3
tanaka-akane <akapengin.g@gmail.com>
2022-12-22 17:05:59 +0900

    ブログ投稿削除機能

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,16 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use HasFactory;
+    use SoftDeletes;
         
     protected $fillable = [
         'title',
         'body',
-    ];
-    
-    use HasFactory;
+        ];
     
     public function getPaginateByLimit(int $limit_count = 10){
     // updated_atで降順に並べたあと、limitで件数制限をかける
